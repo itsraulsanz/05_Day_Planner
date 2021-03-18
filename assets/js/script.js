0091fe816cd80b9d677bccadf33cad3fa9b97789
Raul Sanz <luar_znas@hotmail.com>
2021-03-18 19:49:04 +0000

    added rest of hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,17 @@ var activityInput = "#activity";
 $(".saveBtn").click(function() {
     var activity = $(this).siblings('.description').val();
     var timeActivity = $(this).parent().attr('id');
-    console.log(activity)
-    console.log(timeActivity)
     localStorage.setItem(timeActivity, activity)
-    //alert('saved');
+ 
 });
 
 $('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
+$('#18 .description').val(localStorage.getItem('18'));
