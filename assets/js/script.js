874d2179e48dbe3edde8285b00a0f6352bd2d437
Raul Sanz <luar_znas@hotmail.com>
2021-03-18 19:37:59 +0000

    added button submit to the local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 // Displaying the day
 var reformatDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(reformatDate);
-
+var hour = moment().hours();
 
 // Print activity
-var activityInput = $('#activity');
 
-// var printActivity = function ()
+var activityTime = "#activityHour";
+var activityInput = "#activity";
+
+// Save time and activity
+$(".saveBtn").click(function() {
+    var activity = $(this).siblings('.description').val();
+    var timeActivity = $(this).parent().attr('id');
+    console.log(activity)
+    console.log(timeActivity)
+    localStorage.setItem(timeActivity, activity)
+    //alert('saved');
+});
+
+$('#9 .description').val(localStorage.getItem('9'));
