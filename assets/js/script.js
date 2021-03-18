51102efbf137d59f39e3d2bbd1a1f2d47c01b1f1
Raul Sanz <luar_znas@hotmail.com>
2021-03-18 17:49:48 +0000

    adding the table, style, textarea and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@
 var reformatDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(reformatDate);
 
+
+// Print activity
+var activityInput = $('#activity');
+
+// var printActivity = function ()
