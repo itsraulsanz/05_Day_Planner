f43c16399451ded6bf5c8b8fa3c1d96b1757c894
Raul Sanz <luar_znas@hotmail.com>
2021-03-19 18:48:24 +0000

    CSS: fixed the border. JS: Updating the time each second

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Displaying the day
-var reformatDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-$("#currentDay").text(reformatDate);
-var hour = moment().hours();
+var update = function () {
+  var reformatDate = moment().format("dddd, MMMM Do YYYY, HH:mm:ss a");
+  $("#currentDay").text(reformatDate);
+};
+setInterval(update, 1000);
 
 // Save time and activity
 $(".saveBtn").click(function () {
@@ -14,30 +16,30 @@ $(".saveBtn").click(function () {
 $(".time-block").each(function () {
   //Need to get the id for that time-block you are looking at in each instance
   var timeActivityBlock = $(this).attr("id");
-  console.log(timeActivityBlock);
+  // console.log(timeActivityBlock);
 
   //var hour = now.getHours();
   var currentHour = moment().format("HH");
-  console.log(currentHour);
+  // console.log(currentHour);
   $(this).switchClass("time-block", "past");
 
   //Then use an if statement to check this against the 'hour' variables
 
   //IF its less then add a class or past
   if (timeActivityBlock < currentHour) {
-    console.log("PAST");
+    // console.log("PAST");
     $(this).switchClass("time-block", "past");
 
     //if its equal add a class of present
   }
   if (timeActivityBlock === currentHour) {
-    console.log("PRESENT");
+    // console.log("PRESENT");
     $(this).switchClass("time-block", "present");
 
     //if its future add a class of future
   }
   if (timeActivityBlock > currentHour) {
-    console.log("FUTURE");
+    // console.log("FUTURE");
     $(this).switchClass("time-block", "future");
   }
 });
